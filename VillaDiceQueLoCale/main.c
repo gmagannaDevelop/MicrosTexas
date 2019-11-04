8d1548a9e6413385d70d1575df8ecd4c0c4fcfba
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-11-04 11:02:42 -0600

    :alembic: Cycle modulation.
    .

--- VillaDiceQueLoCale/main.c
+++ VillaDiceQueLoCale/main.c
@@ -1,12 +1,12 @@
 #include "msp430g2553.h"
-#include <stdio.h>
+//#include <stdio.h>
  int main( void )
 {
 // Stop watchdog timer to prevent time out reset WDTCTL = WDTPW + WDTHOLD;
 
   P1DIR = 0x37;
   P2DIR = 0x01;
-  int d = 0;
+  int i = 0, n = 0, d = 0;
   ADC10CTL1 = (INCH_3 | SHS_0);     // Conf convertidor analogico-digital
   //printf("ALV WE : %d", ADC10CTL1);
   ADC10AE0  = 0x08;
@@ -16,37 +16,43 @@
   {
     ADC10CTL0 = (SREF_0 | ADC10SHT_0 | ADC10ON | ENC | ADC10SC);
 
-    while((ADC10IFG&0x04)==0){}
-
     d=ADC10MEM;
 
-    if((d>=0)&&(d<170))
+    //while((ADC10IFG&0x04)==0){}
+    for (i=1000*d; i>0; i--){
+
+    }
+
+    n++;
+
+    if((n>=0)&&(n<170))
     {
         P1OUT=0x01;
         P2OUT=0x00;
     }
-    else if((d>=170)&&(d<340))
+    else if((n>=170)&&(n<340))
     {
         P1OUT=0x02;
         P2OUT=0x00;
     }
-    else if((d>=340)&&(d<510))
+    else if((n>=340)&&(n<510))
     {
         P1OUT=0x04;
         P2OUT=0x00;
     }
-    else if((d>=510)&&(d<680))
+    else if((n>=510)&&(n<680))
     {
         P1OUT=0x10;
         P2OUT=0x00;
     }
-    else if((d>=680)&&(d<850))
+    else if((n>=680)&&(n<850))
     {
         P1OUT=0x20;
         P2OUT=0x00;
     }
     else
     {
+        n = 0;
         P1OUT=0x00;
         P2OUT=0x01;
     }
